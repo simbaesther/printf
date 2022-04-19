9309e9be6fa89fbc8db45c87bf2d294990775ec8
Esther Simba <simbaesther@gmail.com>
2022-04-20 00:06:44 +0300

    non printable characters

--- main.h
+++ main.h
@@ -34,6 +34,7 @@ int unsigned_integer(va_list);
 int print_octal(va_list list);
 int print_hex(va_list list);
 int print_heX(va_list list);
+int print_bigS(va_list l, mods *f)
 
 /*Helper functions*/
 unsigned int base_len(unsigned int, int);
